b2b69dff061b26c17408a16ee73eb8c268f17319
yas-mnkornym <yasuhiro@aurora.ocn.ne.jp>
2016-03-26 01:58:00 +0900

    Initial Commit
    フェイトさんのやつ

---
+++ Source/KancolleVV/Models/KancolleApiSendModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KancolleVV.Models
+{
+    public class KancolleApiSendModel
+    {
+        public string LoginSessionId { get; set; }
+
+        public string AgentId { get; set; }
+
+        public string Path { get; set; }
+
+        public string RequestValue { get; set; }
+
+        public string ResponseValue { get; set; }
+
+        public int? StatusCode { get; set; }
+
+        public string HttpDate { get; set; }
+
+        public string LocalTime { get; set; }
+    }
+}
